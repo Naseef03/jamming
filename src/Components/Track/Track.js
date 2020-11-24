bc50dcd699107c1c4d06408a04b3bcef1c298462
Naseef03 <muhammednaseef03@gmail.com>
2020-11-24 15:32:45 -0800

    Pass Down Search Result and Render Result List

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -5,18 +5,18 @@ import './Track.css';
 export class Track extends React.Component {
     renderAction() {
         if(this.props.isRemoval) {
-            return <button class="Track-action">-</button>
+            return <button className="Track-action">-</button>
         } else {
-            return <button class="Track-action">+</button>
+            return <button className="Track-action">+</button>
         }
     }
     
     render(){
         return(
-            <div class="Track">
-                <div class="Track-information">
-                    <h3>track name will go here</h3>
-                    <p>track artist will go here| track album will go here </p>
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album} </p>
                  </div>
                 {this.renderAction()}
             </div>
